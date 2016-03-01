4d0cbfbbdc7a5340b504e26206ee966c80cd1397
dean thurston <deanthurston2791@gmail.com>
2016-03-01 15:16:01 -0500

    CLEANED UP
    added math function and removed tiled sprite

--- Assets/Scripts/MyMath.cs
+++ Assets/Scripts/MyMath.cs
@@ -8,4 +8,25 @@ public static class MyMath
     {
         return vector - 2 * Vector2.Dot(vector, normal) * normal;
     }
+
+    // returns the distance from a point to a line (desginnated by two points)
+    // returns position or negative depending on what side the point is on
+    public static bool LeftOfLine(Vector2 start, Vector2 end, Vector2 point)
+    {
+        if (start.x != end.x && start.y != end.y)
+        {
+            float tx = (point.x - start.x) / (end.x - start.x);
+            float ty = (point.y - start.y) / (end.y - start.y);
+            Vector2 dir = (end - start).normalized;
+            return tx > ty;
+            // find the t between tx and ty from the perp point; 
+        }
+        else
+        {
+            if (start.x == end.x)
+                return point.x < start.x;
+            else
+                return point.y < start.y;
+        }
+    }
 }
---
+++ Assets/Scripts/TestMath.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMath : MonoBehaviour {
+
+    public Transform start;
+    public Transform end;
+    public Transform marker;
+    public Transform mouseMarker;
+    // Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        Debug.Log(MyMath.LeftOfLine(start.position.XY(), end.position.XY(), mouseMarker.position.XY()));
+        mouseMarker.position = Controls.ClickedPosition().XYZ(mouseMarker.position.z);
+    }
+}
